a57977ac73cbfcc48ce72f1e450e07cbbebf013d
Daniel Szuster <danielszu23@gmail.com>
2023-08-20 17:47:22 +0300

    chore: cleared unused code in whiteboard

--- svelte-whiteboard-app/src/Whiteboard.svelte
+++ svelte-whiteboard-app/src/Whiteboard.svelte
@@ -29,39 +29,6 @@
     secretWord.Text = "";
   }
 
-  function handleMouseDown(event) {
-    if (!canvas) return;
-    const { offsetX, offsetY } = event;
-    drawing = true;
-
-    socket.emit("draw", {
-      type: "start",
-      x: offsetX,
-      y: offsetY,
-      room,
-      color: playerColor,
-    });
-  }
-
-  function handleMouseMove(event) {
-    if (!canvas) return;
-    if (!drawing) return;
-    const { offsetX, offsetY } = event;
-
-    socket.emit("draw", {
-      type: "move",
-      x: offsetX,
-      y: offsetY,
-      room,
-      color: playerColor,
-    });
-  }
-
-  function handleMouseUp(event) {
-    if (!canvas) return;
-    drawing = false;
-  }
-
   function handleCanvasEvent(event: CustomEvent<DrawEventModel>) {
     socket.emit("draw", event.detail);
   }
@@ -70,9 +37,6 @@
     console.log(
       "Create the canvas and set up listeners when the room is joined"
     );
-    // Create the canvas and set up listeners when the room is joined
-    // canvas = document.getElementById("canvas");
-    // context = canvas.getContext("2d");
 
     socket.on("draw", (data) => {
       drawOnCanvas(data);
